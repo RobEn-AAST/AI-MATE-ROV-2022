2b59fda10eef4c650f1fb039507b6b54b4d1136c
MahmoudHany12 <99086974+MahmoudHany12@users.noreply.github.com>
2022-05-10 20:55:02 +0200

    Update mapping.py

--- 3.2_mapping&mosaic/mapping.py
+++ 3.2_mapping&mosaic/mapping.py
@@ -1,1 +1,44 @@
+import cv2
 
+class DrawLineWidget(object):
+    def __init__(self):
+        self.original_image = cv2.imread(r'C:\Users\lenovo\OneDrive\Desktop\Roben\WhatsApp Image 2022-05-09 at 6.54.02 PM.jpeg')
+        self.clone = self.original_image.copy()
+
+        cv2.namedWindow('image')
+        cv2.setMouseCallback('image', self.extract_coordinates)
+
+        # List to store start/end points
+        self.image_coordinates = []
+
+    def extract_coordinates(self, event, x, y, flags, parameters):
+        # Record starting (x,y) coordinates on left mouse button click
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.image_coordinates = [(x,y)]
+
+        # Record ending (x,y) coordintes on left mouse bottom release
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.image_coordinates.append((x,y))
+            print('Starting: {}, Ending: {}'.format(self.image_coordinates[0], self.image_coordinates[1]))
+
+            # Draw line
+            cv2.line(self.clone, self.image_coordinates[0], self.image_coordinates[1], (36,255,12), 2)
+            cv2.imshow("image", self.clone) 
+
+        # Clear drawing boxes on right mouse button click
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            self.clone = self.original_image.copy()
+
+    def show_image(self):
+        return self.clone
+
+if __name__ == '__main__':
+    draw_line_widget = DrawLineWidget()
+    while True:
+        cv2.imshow('image', draw_line_widget.show_image())
+        key = cv2.waitKey(1)
+
+        # Close program with keyboard 'x'
+        if key == ord('x'):
+            cv2.destroyAllWindows()
+            exit(1)
