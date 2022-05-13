cacfd72e85f69c5b7b981a5797a9f62a316c9104
Anton Ashraf <51335125+AntonAshraf@users.noreply.github.com>
2022-05-13 14:22:33 +0200

    Change color of line to brown

--- 3.2_mapping&mosaic/mapping.py
+++ 3.2_mapping&mosaic/mapping.py
@@ -22,7 +22,7 @@ class DrawLineWidget(object):
             print('Starting: {}, Ending: {}'.format(self.image_coordinates[0], self.image_coordinates[1]))
 
             # Draw line
-            cv2.line(self.clone, self.image_coordinates[0], self.image_coordinates[1], (36,255,12), 2)
+            cv2.line(self.clone, self.image_coordinates[0], self.image_coordinates[1], (42, 42, 165), 2)
             cv2.imshow("image", self.clone) 
 
         # Clear drawing boxes on right mouse button click
