47724138ad3d4d73c0ebdd339121a10f90508e37
omar9991 <omar85zaki@gmail.com>
2022-05-18 17:22:04 +0200

    tkinter GUI

--- GUI.py
+++ GUI.py
@@ -1,43 +1,72 @@
-from tkinter import Button
-from turtle import textinput
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from GUIR.mail import mailprint
+import tkinter as tk
+from tkinter import *
+import threading
+import os
 
-class Grid(GridLayout):
-    def __init__(self, **kwargs):
-        super(Grid,self).__init__(**kwargs)
-        self.cols = 2
+# from grid import start_drawing 
 
-        
-        self.submit = Button(text='2.1_flay transectline')
-        self.submit.bind(on_press =self.press)
-        self.add_widget(self.submit)
+cam = "0" # camera 0
+
+def mapping_exec():
+    print("im here ")
+    os.system("python ./mapping_mosaic/mapping.py") 
+    print("im out ")
+
+def Healthy_exec():
+    os.system("python ./mapping_mosaic/mapping.py") 
+
+def MFish_exec():
+    os.system("python ./mapping_mosaic/mapping.py") 
+
+def Fltransectline_exec():
+    os.system("python ./mapping_mosaic/mapping.py") 
+
+def PIDS_exec():
+    os.system("python ./mapping_mosaic/mapping.py") 
+
+
+window = tk.Tk()
+window.geometry('700x700')
+window.title("run rov")
+
+   # the button for transect line mission 
+
+button1 = tk.Button(window, text = "FLYING  \n TRANSECT \nLINE" , fg = "blue", width = 18, height = 8, command = lambda : print('x'))
+button1.place(x=200, y= 120)
+
+   # the button for color detection mission
+
+button2 = tk.Button(window, text = "CORAL COLOR" , fg = "blue", width = 18, height = 8, command = lambda : print('x'))
+button2.place(x=200, y= 300)
+
+
+   # the button for stitching mission
+
+
+button3 = tk.Button(window, text = "STITCHING" , fg = "blue", width = 18, height = 8, command = lambda : print('x'))
+button3.place(x=400, y= 120)
+
+  
+
+label = tk.Label(window, text = "MISSIONS" , fg = "green",font="15")
+label.place(x= 300, y= 50)
 
 
 
-        self.submit = Button(text='2.3_Measure fish size')
-        self.submit.bind(on_press =self.press)
-        self.add_widget(self.submit)
+
+button4 = tk.Button(window, text = "GRID \n TASK" , fg = "blue", width = 18, height = 8)
+button4.place(x=400, y= 300)
+
+button5 = tk.Button(window, text = "Manual\n Coral Reef" , fg = "blue", width = 18, height = 8, command = lambda : print('x'))
+button5.place(x=200, y= 480)
+
+button6 = tk.Button(window, text = "Mapping" , fg = "blue", width = 18, height = 8, command = lambda :mapping_exec())
+button6.place(x=400, y= 480)
 
 
-        self.add_widget(Button(text='Hello 2'))
-        self.add_widget(Button(text='World 2'))
-        self.add_widget(Button(text='Hello 1'))
-        self.add_widget(Button(text='World 1'))
-        self.add_widget(Button(text='Hello 2'))
-        self.add_widget(Button(text='World 2'))
+window.mainloop()
 
-    def press(self , instance):
-        print("pressed") 
 
-class MyApp(App):
-    def build(self):
-        return Grid()
 
-if __name__ == '__main__':
-    MyApp().run()
+
+
