8e4bb5adfc3af7d3b90f0717747ceac9b54cb088
Seifeldin Sayed <seifeldin18102002@gmail.com>
2022-05-19 22:17:10 +0200

    update change names

--- MeasureInFloatV_H/main.py
+++ MeasureInFloatV_H/main.py
@@ -38,25 +38,25 @@ c=Entry(win)
 c.place(x= 100, y= 110)
 
 
-label = Label( win, text="Vertical in km:", font=('Calibri 15') )
+label = Label( win, text="right in km:", font=('Calibri 15') )
 label.place(x= 30, y= 160)
 
 label=Label(win, font=('Calibri 15'))
 label.place(x= 190, y= 160)
 
-labe2 = Label( win, text="horizontal in km:", font=('Calibri 15') )
+labe2 = Label( win, text="up in km:", font=('Calibri 15') )
 labe2.place(x= 30, y= 200)
 
 labe2=Label(win, font=('Calibri 15'))
 labe2.place(x= 190, y= 200)
 
-labe3 = Label( win, text="vertical in square:", font=('Calibri 15') )
+labe3 = Label( win, text="right in square:", font=('Calibri 15') )
 labe3.place(x= 280, y= 160)
 
 labe3=Label(win, font=('Calibri 15'))
 labe3.place(x= 470, y= 160)
 
-labe4 = Label( win, text="horizontal in square:", font=('Calibri 15') )
+labe4 = Label( win, text="up in square:", font=('Calibri 15') )
 labe4.place(x= 280, y= 200)
 
 labe4=Label(win, font=('Calibri 15'))
