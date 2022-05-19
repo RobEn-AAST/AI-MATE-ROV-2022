e573eddabb2e7c9a680406b66f9f8167be03e1af
omar-alsawaf <99086491+omar-alsawaf@users.noreply.github.com>
2022-05-19 04:54:51 +0200

    run code seperately

--- mosaicAndMapping/main.py
+++ mosaicAndMapping/main.py
@@ -5,19 +5,26 @@ from crop import crop_imgs
 from cut2Parts import split_img
 from time import sleep
 from random8 import Display8imgs
+from mapping import DrawLineWidget
 import cv2
 
-def show_images():
-    imgs_list = save()
-    imgs_cropped_list = crop_imgs(imgs_list)
-    img8_list = split_img(imgs_cropped_list)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return img8_list
+def mapping():
+    draw_line_widgetk = DrawLineWidget()
+    while True:
+        cv2.imshow('image', draw_line_widgetk.show_image())
+        key = cv2.waitKey(10)
+
+        # Close program with keyboard 'esc'
+        if key == 27:
+            cv2.destroyAllWindows()
+            break
 
 
 if __name__=='__main__':
-    img8_list = show_images()
-    sleep(2)
+    imgs_list = save()
+    imgs_cropped_list = crop_imgs(imgs_list)
+    img8_list = split_img(imgs_cropped_list)
+    sleep(1)
+    mapping()
     Display8imgs(img8_list)
     mosaic(img8_list)
