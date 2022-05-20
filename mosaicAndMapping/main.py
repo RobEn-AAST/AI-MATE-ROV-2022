9f98ffb23ba4bccabaf0b271b282e309d7ea2efa
karim kohel <kareemkohel@gmail.com>
2022-05-20 07:55:34 +0200

    removed dev code in mosaic mission

--- mosaicAndMapping/main.py
+++ mosaicAndMapping/main.py
@@ -1,6 +1,6 @@
 import imp
 from mosaic import mosaic
-# from PressSave import save
+from PressSave import save
 from crop import crop_imgs
 from cut2Parts import split_img
 from time import sleep
@@ -22,10 +22,9 @@ def mapping():
 
 
 if __name__=='__main__':
-    # imgs_list = save()
-    imgs_list = [cv2.imread(r'D:\RobEn\python\ctd-main\AI-MATE-ROV-2022\mosaicAndMapping\1.jpeg'), cv2.imread(r'D:\RobEn\python\ctd-main\AI-MATE-ROV-2022\mosaicAndMapping\2.jpeg'), cv2.imread(r'D:\RobEn\python\ctd-main\AI-MATE-ROV-2022\mosaicAndMapping\3.jpeg'), cv2.imread(r'D:\RobEn\python\ctd-main\AI-MATE-ROV-2022\mosaicAndMapping\4.jpeg')]
+    imgs_list = save()
     imgs_cropped_list = crop_imgs(imgs_list)
-    rotated = torotate (imgs_cropped_list)
+    rotated = torotate(imgs_cropped_list)
     img8_list = split_img(imgs_cropped_list)
     sleep(1)
     mapping()
